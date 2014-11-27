ae35609297f6c02542d56025e681541ee07a34e3
straup <github@aaronland.net>
2014-11-27 08:53:32 -0500

    standardize on ucd-response

--- bin/ucd-server.go
+++ bin/ucd-server.go
@@ -8,10 +8,48 @@ import (
 	"org.cooperhewitt/ucd/names"
 )
 
+type UCDResponse struct {
+     Chars []names.UCDName
+}
+
 func string(w http.ResponseWriter, r *http.Request) {
 
 	txt := r.FormValue("text")
-	rsp := names.NamesForString(txt)
+	chars := names.NamesForString(txt)
+
+	rsp := UCDResponse{chars}
+	send(w, r, rsp)
+}
+
+func char(w http.ResponseWriter, r *http.Request) {
+
+	txt := r.FormValue("text")
+	char := names.Name(txt)
+
+	chars := make([]names.UCDName, 1)
+	chars[0] = char
+
+	rsp := UCDResponse{chars}
+	send(w, r, rsp)
+}
+
+func send(w http.ResponseWriter, r *http.Request, rsp UCDResponse) {
+
+	send_json(w, rsp)
+}
+
+/*
+func send_text(w http.ResponseWriter, rsp UCDResponse) {
+
+     w.Header().Set("Content-Type", "application/json")
+
+     for _, char := range rsp.Chars {
+     	 fmt.Fprintf(w ,char)
+     }     
+}
+*/
+
+func send_json(w http.ResponseWriter, rsp UCDResponse) {
 
 	js, err := json.Marshal(rsp)
 
@@ -22,13 +60,6 @@ func string(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(js)
-}
-
-func char(w http.ResponseWriter, r *http.Request) {
-
-	txt := r.FormValue("text")
-	ucd := names.Name(txt)
-	fmt.Fprintf(w, ucd.Name)
 }
 
 func main() {
